b5445f6847c0f4c8ef10839b2b32ded18b65e92d
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-09 02:37:43 +1000

    Grouped card codes that have the same name in the error output of unique name tests

--- test/name/unique.py
+++ test/name/unique.py
@@ -5,12 +5,21 @@ import greencard
 def name_unique(library):
     """Check that card names are unique."""
     with library.connection() as lcon:
-        result = lcon.execute("""SELECT code FROM CARDS WHERE name IN (
+        result = lcon.execute("""SELECT code, name FROM CARDS WHERE name IN (
         SELECT name FROM CARDS GROUP BY name HAVING (COUNT(name ) > 1)
         )""").fetchall()
 
-        if len(result):
-            print("The following have duplicate names.")
-            for code in result:
-                print("    {0}".format(code[0]))
-            assert False
+        if len(result) == 0:
+            return None
+
+        dupes = {}
+        for code, name in result:
+            if name not in dupes:
+                dupes[name] = [code]
+            else:
+                dupes[name].append(code)
+
+        print("\nThe following lists of card codes share the same name.")
+        for codes in dupes.values():
+            print("    ", codes)
+        assert False
